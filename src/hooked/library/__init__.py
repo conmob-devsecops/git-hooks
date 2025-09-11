b17db333fd299d42d273f3780b3b64f79cfc62cb
Sebastian Gurkasch <sebastian.gurkasch@t-systems.com>
2025-09-11 16:02:15 +0200

    wip snapshot for installer test

--- src/hooked/library/__init__.py
+++ src/hooked/library/__init__.py
@@ -5,4 +5,4 @@ from .files import get_base_dir, create_base_dir, create_hooks_dir, copy_git_hoo
 from .git import git_set_global_hook_path, git_unset_global_hook_path, git_set_template_dir, git_unset_template_dir, git_get_tags, git_get_last_branch_commit
 from .upgrade import self_upgrade
 from .logger import logger
-from .pre_commit_diif import pre_commit_diff
+from .pre_commit_diff import pre_commit_diff
